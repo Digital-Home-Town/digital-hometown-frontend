f9c2d969d27b54954faef818752978a31827069e
bdnkl <boas_d@web.de>
2022-01-29 11:05:12 +0100

    Update App.test.tsx

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,6 +1,12 @@
+import React from "react"
+
+import { render } from "@testing-library/react"
+
+import App from "./App"
+
 describe("App component", () => {
   it("renders learn react link", () => {
-    // render(<App />)
+    render(<App />)
     // const linkElement = screen.getByText(/learn react/i)
     // expect(linkElement).toBeInTheDocument()
   })
