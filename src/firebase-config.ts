8b31e1423757f7bcf8d38e033bde1bae97cd19a7
jonasdero <jonas.roser@gmail.com>
2022-04-01 16:56:45 +0200

    add firebase config

---
+++ src/firebase-config.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAsI2qLoPZpcA5QFp1Dnz8TiNbrCqr8XNk",
+  authDomain: "digital-dahoam.firebaseapp.com",
+  projectId: "digital-dahoam",
+  storageBucket: "digital-dahoam.appspot.com",
+  messagingSenderId: "532207686252",
+  appId: "1:532207686252:web:5f455be6b61e787d11fe49",
+  measurementId: "G-RB98YSRV64",
+}
+
+export const app = initializeApp(firebaseConfig)
