600a2777637b7b3a8b4147896caef077fe70fadd
jonasdero <jonas.roser@gmail.com>
2022-04-01 16:46:15 +0200

    DH-40 Added first simple authentication
    AuthContext: central place to access logged in user information
    withAuth: React HOC to insert user information as a prop
    
    # Conflicts:
    #	src/components/App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css"
 
-import { Box, Typography } from "@mui/material"
+import { Box } from "@mui/material"
 import Container from "@mui/material/Container"
 import React from "react"
 import { HashRouter, Route, Routes } from "react-router-dom"
@@ -8,19 +8,63 @@ import { HashRouter, Route, Routes } from "react-router-dom"
 import { INITIAL_LOGGED_IN_USER } from "../auth/Auth"
 import { AuthProvider } from "../auth/AuthContext"
 import { ThemeContextProvider } from "../context/ThemeContext"
+import { AuthContextValues } from "../contexts/AuthContext"
+import withAuth from "../hocs/withAuth"
 import { useBackendHealth } from "../hooks/useBackendHealth"
-import Footer from "./Footer"
-import Header from "./Header"
-import MuiPlayground from "./playground/mui/MuiPlayground"
-import SignIn from "./SignIn"
-import SignOut from "./SignOut"
-import Start from "./Start"
+import { INITIAL_LOGGED_IN_USER } from "../types/User"
 
-function BackendHealth() {
+function ReactStartPage() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Digital Hometown Frontend
+        {/* <br /> */}
+        {/* {new Date().toLocaleString()} */}
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+      <a href="https://startpage.com">Jan</a>
+      <a href="https://www.google.com">Jonas was here</a>
+      <a href="https://github.com/bdnkl">Der hier auch</a>
+    </header>
+  )
+}
+
+function BackendHealthNoAuth({ loggedInUser, setLoggedInUser }: AuthContextValues) {
   const { status } = useBackendHealth("LOADING")
 
-  return <Typography sx={{ textAlign: "center" }}>Backend status is {status.status}</Typography>
+  console.log("user")
+  return loggedInUser !== undefined ? (
+    <div>
+      <p>Backend status is {status.status}</p>
+      <p>
+        {loggedInUser.name.first} {loggedInUser.name.last} is logged in as {loggedInUser.role}
+      </p>
+      <button
+        onClick={() => {
+          setLoggedInUser(undefined)
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  ) : (
+    <div>
+      <p>Not authenticated</p>
+      <button
+        onClick={() => {
+          setLoggedInUser(INITIAL_LOGGED_IN_USER)
+        }}
+      >
+        Login
+      </button>
+    </div>
+  )
 }
+
+const BackendHealth = withAuth(BackendHealthNoAuth)
 
 function App() {
   return (
