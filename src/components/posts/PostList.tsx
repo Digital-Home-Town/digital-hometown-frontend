b781a21e0a70ab3a7287e1a1b56ce859b2f569cc
jonasdero <jonas.roser@gmail.com>
2022-10-29 12:17:20 +0200

    filter posts of blocked users

--- src/components/posts/PostList.tsx
+++ src/components/posts/PostList.tsx
@@ -3,17 +3,17 @@ import * as React from "react"
 import { Navigate } from "react-router"
 import { AuthContextI } from "src/auth/AuthContext"
 import withAuth from "src/auth/withAuth"
-import usePostContext from "./PostContext"
 
+import usePostContext from "./PostContext"
 import Posts from "./Posts"
 
 function PostList({ currentUser }: AuthContextI) {
   const { posts } = usePostContext()
-
+  const filteredPosts = posts.filter((post) => !currentUser?.blocked?.includes(post.author.id))
   return currentUser ? (
     <Box sx={{ mt: 1 }}>
       <h1>Alle Beiträge</h1>
-      <Posts posts={posts}></Posts>
+      <Posts posts={filteredPosts}></Posts>
     </Box>
   ) : (
     <Navigate to="/sign-in" />
